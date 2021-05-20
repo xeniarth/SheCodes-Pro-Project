ede8ddfc73e166d251fa2297dd1680d4db61fba7
xeniarth <84279632+xeniarth@users.noreply.github.com>
2021-05-20 16:34:03 +0200

    Call Icon via API

--- src/index.js
+++ src/index.js
@@ -12,24 +12,8 @@ function search(input) {
 
 search("Zurich");
 
-function displaySearchedCityTemp(response) {
-  let location = response.data.name;
-  let minTemp = Math.round(response.data.main.temp_min);
-  let maxTemp = Math.round(response.data.main.temp_max);
-  let humidity = response.data.main.humidity;
-  let windspeed = Math.round(response.data.wind.speed);
-  document.querySelector("h1").innerHTML = location;
-  document.querySelector("#min-temp").innerHTML = `${minTemp}`;
-  document.querySelector("#max-temp").innerHTML = `${maxTemp}`;
-  document.querySelector("#current-humidity").innerHTML = `${humidity}%`;
-  document.querySelector("#current-wind").innerHTML = `${windspeed} km/h`;
-}
-
-let cityInput = document.querySelector("#city-search-form");
-cityInput.addEventListener("submit", handleSubmit);
-
-function displayCurrentDate() {
-  let now = new Date();
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
   let days = [
     "Sunday",
     "Monday",
@@ -37,7 +21,7 @@ function displayCurrentDate() {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
   let months = [
     "January",
@@ -51,8 +35,16 @@ function displayCurrentDate() {
     "September",
     "October",
     "November",
-    "December"
+    "December",
   ];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let day = days[now.getDay()];
   let date = now.getDate();
   let month = months[now.getMonth()];
@@ -60,8 +52,36 @@ function displayCurrentDate() {
   document.querySelector(
     "#date-today"
   ).innerHTML = `${day}, ${date}. ${month} ${year}`;
+  document.querySelector(
+    "#time-now"
+  ).innerHTML = `Last updated: ${hours}:${minutes}`;
 }
-displayCurrentDate();
+
+function displaySearchedCityTemp(response) {
+  let location = response.data.name;
+  let minTemp = Math.round(response.data.main.temp_min);
+  let maxTemp = Math.round(response.data.main.temp_max);
+  let humidity = response.data.main.humidity;
+  let windspeed = Math.round(response.data.wind.speed);
+  let date = formatDate(response.data.dt * 1000);
+  let icon = response.data.weather[0].icon;
+  let description = response.data.weather[0].description;
+  console.log(icon);
+  document.querySelector("h1").innerHTML = location;
+  document.querySelector("#min-temp").innerHTML = `${minTemp}`;
+  document.querySelector("#max-temp").innerHTML = `${maxTemp}`;
+  document.querySelector("#current-humidity").innerHTML = `${humidity}%`;
+  document.querySelector("#current-wind").innerHTML = `${windspeed} km/h`;
+  document.querySelector("#date-today");
+  innerHTML = date;
+  document
+    .querySelector("#weather-icon")
+    .setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+  document.querySelector("#weather-icon").setAttribute("alt", `${description}`);
+}
+
+let cityInput = document.querySelector("#city-search-form");
+cityInput.addEventListener("submit", handleSubmit);
 
 function showCelsiusTemp(event) {
   event.preventDefault();
