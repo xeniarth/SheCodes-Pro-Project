09d52ee0a741582013a98fa99ebc92606e47c06f
xeniarth <84279632+xeniarth@users.noreply.github.com>
2021-06-13 19:03:34 +0200

    Add Forecast

--- src/index.js
+++ src/index.js
@@ -55,6 +55,90 @@ function formatDate(timestamp) {
   document.querySelector(
     "#time-now"
   ).innerHTML = `Last updated: ${hours}:${minutes}`;
+
+  document.querySelector("#day-f1").innerHTML = days[now.getDay() + 1];
+  document.querySelector("#day-f2").innerHTML = days[now.getDay() + 2];
+  document.querySelector("#day-f3").innerHTML = days[now.getDay() + 3];
+  document.querySelector("#day-f4").innerHTML = days[now.getDay() + 4];
+  document.querySelector("#day-f5").innerHTML = days[now.getDay() + 5];
+}
+
+function displayForecast(response) {
+  let minTempF1 = Math.round(response.data.daily[0].temp.min);
+  let maxTempF1 = Math.round(response.data.daily[0].temp.max);
+  let humidityF1 = response.data.daily[0].humidity;
+  let windspeedF1 = Math.round(response.data.daily[0].wind_speed);
+  let iconF1 = response.data.daily[0].weather[0].icon;
+  let descriptionF1 = response.data.daily[0].weather[0].description;
+  document.querySelector("#temp-f1").innerHTML = `${minTempF1}°/${maxTempF1}°`;
+  document.querySelector("#hum-f1").innerHTML = `${humidityF1}%`;
+  document.querySelector("#wind-f1").innerHTML = `${windspeedF1} km/h`;
+  document
+    .querySelector("#icon-f1")
+    .setAttribute("src", `http://openweathermap.org/img/wn/${iconF1}@2x.png`);
+  document.querySelector("#icon-f1").setAttribute("alt", `${descriptionF1}`);
+
+  let minTempF2 = Math.round(response.data.daily[1].temp.min);
+  let maxTempF2 = Math.round(response.data.daily[1].temp.max);
+  let humidityF2 = response.data.daily[1].humidity;
+  let windspeedF2 = Math.round(response.data.daily[1].wind_speed);
+  let iconF2 = response.data.daily[1].weather[0].icon;
+  let descriptionF2 = response.data.daily[1].weather[0].description;
+  document.querySelector("#temp-f2").innerHTML = `${minTempF2}°/${maxTempF2}°`;
+  document.querySelector("#hum-f2").innerHTML = `${humidityF2}%`;
+  document.querySelector("#wind-f2").innerHTML = `${windspeedF2} km/h`;
+  document
+    .querySelector("#icon-f2")
+    .setAttribute("src", `http://openweathermap.org/img/wn/${iconF2}@2x.png`);
+  document.querySelector("#icon-f2").setAttribute("alt", `${descriptionF2}`);
+
+  let minTempF3 = Math.round(response.data.daily[2].temp.min);
+  let maxTempF3 = Math.round(response.data.daily[2].temp.max);
+  let humidityF3 = response.data.daily[2].humidity;
+  let windspeedF3 = Math.round(response.data.daily[2].wind_speed);
+  let iconF3 = response.data.daily[2].weather[0].icon;
+  let descriptionF3 = response.data.daily[2].weather[0].description;
+  document.querySelector("#temp-f3").innerHTML = `${minTempF3}°/${maxTempF3}°`;
+  document.querySelector("#hum-f3").innerHTML = `${humidityF3}%`;
+  document.querySelector("#wind-f3").innerHTML = `${windspeedF3} km/h`;
+  document
+    .querySelector("#icon-f3")
+    .setAttribute("src", `http://openweathermap.org/img/wn/${iconF3}@2x.png`);
+  document.querySelector("#icon-f3").setAttribute("alt", `${descriptionF3}`);
+
+  let minTempF4 = Math.round(response.data.daily[3].temp.min);
+  let maxTempF4 = Math.round(response.data.daily[3].temp.max);
+  let humidityF4 = response.data.daily[3].humidity;
+  let windspeedF4 = Math.round(response.data.daily[3].wind_speed);
+  let iconF4 = response.data.daily[3].weather[0].icon;
+  let descriptionF4 = response.data.daily[3].weather[0].description;
+  document.querySelector("#temp-f4").innerHTML = `${minTempF4}°/${maxTempF4}°`;
+  document.querySelector("#hum-f4").innerHTML = `${humidityF4}%`;
+  document.querySelector("#wind-f4").innerHTML = `${windspeedF4} km/h`;
+  document
+    .querySelector("#icon-f4")
+    .setAttribute("src", `http://openweathermap.org/img/wn/${iconF4}@2x.png`);
+  document.querySelector("#icon-f4").setAttribute("alt", `${descriptionF4}`);
+
+  let minTempF5 = Math.round(response.data.daily[4].temp.min);
+  let maxTempF5 = Math.round(response.data.daily[4].temp.max);
+  let humidityF5 = response.data.daily[4].humidity;
+  let windspeedF5 = Math.round(response.data.daily[4].wind_speed);
+  let iconF5 = response.data.daily[4].weather[0].icon;
+  let descriptionF5 = response.data.daily[4].weather[0].description;
+  document.querySelector("#temp-f5").innerHTML = `${minTempF5}°/${maxTempF5}°`;
+  document.querySelector("#hum-f5").innerHTML = `${humidityF5}%`;
+  document.querySelector("#wind-f5").innerHTML = `${windspeedF5} km/h`;
+  document
+    .querySelector("#icon-f5")
+    .setAttribute("src", `http://openweathermap.org/img/wn/${iconF5}@2x.png`);
+  document.querySelector("#icon-f5").setAttribute("alt", `${descriptionF5}`);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "e93661b3ab177c374da4a62247b4b062";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displaySearchedCityTemp(response) {
@@ -66,7 +150,6 @@ function displaySearchedCityTemp(response) {
   let date = formatDate(response.data.dt * 1000);
   let icon = response.data.weather[0].icon;
   let description = response.data.weather[0].description;
-  console.log(icon);
   document.querySelector("h1").innerHTML = location;
   document.querySelector("#min-temp").innerHTML = `${minTemp}`;
   document.querySelector("#max-temp").innerHTML = `${maxTemp}`;
@@ -79,43 +162,12 @@ function displaySearchedCityTemp(response) {
     .querySelector("#weather-icon")
     .setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
   document.querySelector("#weather-icon").setAttribute("alt", `${description}`);
+
+  getForecast(response.data.coord);
 }
 
 let cityInput = document.querySelector("#city-search-form");
 cityInput.addEventListener("submit", handleSubmit);
-
-function showCelsiusTemp(event) {
-  event.preventDefault();
-  unitConversion("metric");
-  celsiusTemp.classList.add("active");
-  fahrenheitTemp.classList.remove("active");
-}
-
-function showFahrenheitTemp(event) {
-  event.preventDefault();
-  unitConversion("imperial");
-  celsiusTemp.classList.remove("active");
-  fahrenheitTemp.classList.add("active");
-}
-
-function displayConvTemp(response) {
-  let minTemp = Math.round(response.data.main.temp_min);
-  let maxTemp = Math.round(response.data.main.temp_max);
-  document.querySelector("#min-temp").innerHTML = `${minTemp}`;
-  document.querySelector("#max-temp").innerHTML = `${maxTemp}`;
-}
-
-function unitConversion(unit) {
-  let input = document.getElementById("header").textContent;
-  let apiKey = "e93661b3ab177c374da4a62247b4b062";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(displayConvTemp);
-}
-
-let celsiusTemp = document.querySelector("#celsius-link");
-celsiusTemp.addEventListener("click", showCelsiusTemp);
-let fahrenheitTemp = document.querySelector("#fahrenheit-link");
-fahrenheitTemp.addEventListener("click", showFahrenheitTemp);
 
 function locateCity(position) {
   let latitude = position.coords.latitude;
